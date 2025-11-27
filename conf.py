46aae8e74b1898794e657356266265e1b6ca2df8
Anas Neumann <anas.neumann.isamm@gmail.com>
2025-11-27 18:55:33 +0100

    reduce impact of the optimizer

--- conf.py
+++ conf.py
@@ -17,11 +17,13 @@ GNN_STACK_SIZE: int      = 2
 DROPOUT_RATE: float      = 0.15
 
 MEMORY_CAPACITY: int     = 50_000 # number of transitions in the replay memory
-NB_EPISODES: int         = 6_000  # number of training/solving episodes
+NB_EPISODES: int         = 3_000  # number of training/solving episodes
 BATCH_SIZE: int          = 128    # size of each batch sampled from the replay memory
 TOP_K: int               = 6      # number of top-Q actions to consider in the Boltzmann exploration
 TEMPERATURE: float       = 0.95   # temperature parameter for the Boltzmann exploration
 LR: float                = 1e-4   # learning rate of AdamW (Init value: 1e-4, 5e-5, 2.5e-5, 1.25e-5, 6.25e-6)
+MIN_LR: float            = 1e-5   # min learning rate
+PATIENCE: int            = 300    # number of episode without loss improvement before reducing the learning rate
 EPS_START: float         = 0.99   # starting value of epsilon
 EPS_END: float           = 0.005  # final value of epsilon
 EPS_DECAY: int           = 500    # controls the rate of exponential decay of epsilon, higher means a slower decay (≈35%)
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from torch.optim import AdamW
 from torch_geometric.data import HeteroData
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from conf import INTERACTIVE, LR, NB_EPISODES, EPS_DECAY, EPS_END, EPS_START, GREEDY_RATE, TOP_K, INFINITY
+from conf import INTERACTIVE, LR, NB_EPISODES, EPS_DECAY, EPS_END, EPS_START, GREEDY_RATE, TOP_K, INFINITY, MIN_LR, PATIENCE
 from src.common import display_final_computing_time
 from src.state import State
 from src.neural_nets import HyperGraphGNN
@@ -93,7 +93,7 @@ def solve(path: str, instance_type: str, instance_name: str, interactive: bool):
     _POLICY_NET.train()
     _TARGET_NET.eval()
     _OPTIMIZER: AdamW             = AdamW(_POLICY_NET.parameters(), lr=LR, amsgrad=True)
-    _SCHEDULER: ReduceLROnPlateau = ReduceLROnPlateau(_OPTIMIZER, mode='min', factor=0.5, patience=100)
+    _SCHEDULER: ReduceLROnPlateau = ReduceLROnPlateau(_OPTIMIZER, mode='min', factor=0.5, patience=PATIENCE, min_lr=MIN_LR)
     for _episode in range(1, NB_EPISODES+1):
         _e: float                                 = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * _episode / EPS_DECAY)
         transition: Transition                    = None
