First fix for delta computation

main.py
@@ -29,7 +29,7 @@ __author__  = "Anas Neumann - anas.neumann@polymtl.ca"
 __version__ = "1.0.0"
 __license__ = "MIT License"
 
-def diversify(memory: ITree, current_transition: Transition, possible_actions: list[PossibleAction], device: str, best_known_Cmax: int, accept_seen: bool = False) -> tuple[Tensor, int, int, bool]:
+def diversify(memory: ITree, current_transition: Transition, possible_actions: list[PossibleAction], device: str, best_known_Cmax: int, accept_seen: bool = False) -> tuple[Tensor, int, bool]:
     """
         Select a random feasible action that has neven been tried yet in the current branch of the tree (but LB <= best_known_Cmax)
     """
@@ -59,15 +59,15 @@ def diversify(memory: ITree, current_transition: Transition, possible_actions: l
         unseen.sort(key=lambda a: a.lb)
         idx: int               = np.random.choice(min(TOP_K, len(unseen)))
         action: PossibleAction = unseen[idx]
-        return torch.tensor([[action.id]], device=device, dtype=torch.long), action.lb, action.ub, fail
-    return None, -1, -1, True
+        return torch.tensor([[action.id]], device=device, dtype=torch.long), action.lb, fail
+    return None, -1, True
 
 def search_possible_actions(state: State, current_transition: Transition, best_Cmax: int, cut_bad_branches: bool = False) -> list[PossibleAction]:
     """
         Search for possible action, but also cut bad branches and update parent LB
     """
     if current_transition is not None:
-        current_transition.refine_from_possible_children(state.init_lb, state.init_ub)
+        current_transition.refine_from_possible_children(state.init_lb)
         if cut_bad_branches:
             current_transition.possible_actions = [a for a in current_transition.possible_actions if a.lb < best_Cmax]
         return current_transition.possible_actions
@@ -75,9 +75,9 @@ def search_possible_actions(state: State, current_transition: Transition, best_C
         possible_actions: list[PossibleAction] = []
         feasible_tasks: list[dict] = find_feasible_tasks(state.tasks, state.scheduled_tasks)
         for task in feasible_tasks:
-            with HypotheticalStep(state, task["Id"]) as step:
+            with HypotheticalStep(state, task["Id"], state.init_ub) as step:
                 if step.success:
-                    possible_actions.append(PossibleAction(id=task["Id"], lb=state.compute_lower_bound(), ub=state.compute_upper_bound()))
+                    possible_actions.append(PossibleAction(id=task["Id"], lb=state.compute_lower_bound()))
         return possible_actions
 
 def solve(path: str, instance_type: str, instance_name: str, interactive: bool):
@@ -123,40 +123,34 @@ def solve(path: str, instance_type: str, instance_name: str, interactive: bool):
         for step in count():
             should_diversify: bool = _search_transition and (step == diversified_step)
             if should_diversify:
-                _action_idx, LB, UB, failed = diversify(memory=_TREE, current_transition=transition, possible_actions=possible_actions, device=_device, best_known_Cmax=Cmax, accept_seen=_search_transition)
+                _action_idx, LB, failed = diversify(memory=_TREE, current_transition=transition, possible_actions=possible_actions, device=_device, best_known_Cmax=Cmax, accept_seen=_search_transition)
                 if failed:
                     should_diversify  = False
                     diversified_step += 1
             if not should_diversify:
-                _action_idx, LB, UB = select_action(state=_state, policy_net=_POLICY_NET, e=_e, greedy=random.random() < GREEDY_RATE, device=_device, memory=_REPLAY_MEMORY, possible_actions=possible_actions)
+                _action_idx, LB = select_action(state=_state, policy_net=_POLICY_NET, e=_e, greedy=random.random() < GREEDY_RATE, device=_device, memory=_REPLAY_MEMORY, possible_actions=possible_actions)
             if _search_transition:
-                transition        = _TREE.search_transition(action=_action_idx.item(), current_transition=transition)
+                transition         = _TREE.search_transition(action=_action_idx.item(), current_transition=transition)
                 _search_transition = transition is not None
             _steps                += 1
-            take_step(state=_state, action=_action_idx.item())
+            take_step(state=_state, action=_action_idx.item(), ub=_state.init_ub)
             possible_actions       = search_possible_actions(state=_state, current_transition=transition, best_Cmax=Cmax, cut_bad_branches=_cut_bad_branches)
             if not possible_actions and not _state.done:
                 print(f"-> ERROR: No possible actions with lower bound < current best Cmax ({Cmax})!")
                 break
             _next_graph: HeteroData = _state.to_hyper_graph(possible_actions=possible_actions, transitions=transition.next if transition is not None else [])
             _state.lower_bound      = LB
-            _state.upper_bound      = min(_state.upper_bound, UB)
             if _transitions_in_episode:
-                _transitions_in_episode[-1].lb = max(_transitions_in_episode[-1].lb, LB)  
-                _transitions_in_episode[-1].ub = min(_transitions_in_episode[-1].ub, UB)
-            delta_lb: int = LB - (_transitions_in_episode[-1].lb if _transitions_in_episode else _state.init_lb)
-            delta_ub: int = _state.upper_bound - (_transitions_in_episode[-1].ub if _transitions_in_episode else _state.init_ub)
-            #if delta_lb != 0:
-            #    print(delta_lb, "...DELTA LB")
-            if delta_ub != 0:
-                print(delta_ub, "...DELTA UB")
+                _transitions_in_episode[-1].lb = min(_transitions_in_episode[-1].lb, LB)  
+                delta_lb: int                  = LB - _transitions_in_episode[-1].lb
+            else:
+                _state.init_lb = min(_state.init_lb, LB)
+                delta_lb: int  = LB - _state.init_lb          
             _transitions_in_episode.append(Transition(action=_action_idx, 
                                                       previous_graph=_state.graph, 
                                                       graph=_next_graph, 
                                                       lb=_state.lower_bound, 
                                                       delta_lb=delta_lb, 
-                                                      ub=_state.upper_bound,
-                                                      delta_ub=delta_ub, 
                                                       possible_actions=possible_actions,
                                                       parent=_transitions_in_episode[-1] if _transitions_in_episode else None))
             _state.graph = _next_graph

src/dqn_functions.py
@@ -51,7 +51,7 @@ def ssgs(tasks: list[dict], resources: list[tuple[int, int]], task: dict, ub: in
     for predecessor in predecessors:
         if predecessor["Finish"] >= min_start_day:
             min_start_day = predecessor["Finish"] + (not (not (predecessor["Duration"] * task["Duration"])))
-    start_day = find_possible_start_day_for_task(tasks, resources, task, min_start_day, ub)
+    start_day = find_possible_start_day_for_task(tasks, resources, task, min_start_day, ub * 2)
     if start_day > 0:
         task["Start"]  = start_day
         task["Finish"] = start_day + task["Duration"] - (not (not (task["Duration"])))
@@ -73,7 +73,7 @@ class HypotheticalStep:
         Context manager to temporarily apply a task to the state, 
         compute metrics, and revert the state exactly as it was.
     """
-    def __init__(self, state: State, task_id: int):
+    def __init__(self, state: State, task_id: int, ub: int):
         self.state: State        = state
         self.task_id: int        = task_id
         self.task: dict          = None
@@ -82,6 +82,7 @@ class HypotheticalStep:
         self.old_makespan: int   = state.make_span
         self.was_scheduled: bool = False
         self.success: bool       = False
+        self.ub: int             = ub
 
     def __enter__(self):
         self.task       = next(t for t in self.state.tasks if t["Id"] == self.task_id)
@@ -93,7 +94,7 @@ class HypotheticalStep:
                 feasible = False
                 break
         if feasible:
-            updated_task = ssgs(self.state.tasks, self.state.resources, self.task, 10000)
+            updated_task = ssgs(self.state.tasks, self.state.resources, self.task, self.ub)
             if updated_task["Start"] > 0:
                 self.state.scheduled_tasks.append(self.task_id)
                 self.state.make_span = max(self.state.make_span, updated_task["Finish"])
@@ -108,7 +109,7 @@ class HypotheticalStep:
             self.task["Start"]   = self.old_start
             self.task["Finish"]  = self.old_finish
 
-def take_step(state: State, action: int):
+def take_step(state: State, action: int, ub: int):
     """
         Take a step in the environment by selecting an action (task) to schedule
     """
@@ -118,7 +119,7 @@ def take_step(state: State, action: int):
         print(f"{action} not found")
         exit()
     feasible: bool = check_precedence_feasibility(state, task)
-    task: dict     = ssgs(state.tasks, state.resources, task, 10000)
+    task: dict     = ssgs(state.tasks, state.resources, task, ub)
     if not feasible or task["Start"] <= 0:
         build_impossible_state(state, task)
     else:
@@ -130,7 +131,7 @@ def take_step(state: State, action: int):
 
 mps_amp = (torch.autocast(device_type="mps", dtype=torch.float16) if torch.backends.mps.is_available() else nullcontext())
 
-def select_action(state: State, policy_net: HyperGraphGNN, e: float, greedy: bool, possible_actions: list[PossibleAction], device: Device, memory: Memory=None) -> tuple[Tensor, int, int]:
+def select_action(state: State, policy_net: HyperGraphGNN, e: float, greedy: bool, possible_actions: list[PossibleAction], device: Device, memory: Memory=None) -> tuple[Tensor, int]:
     """
         Select a feasible-only action using the current policy network OR random (when replay memory is still relatively empty)
     """
@@ -154,8 +155,8 @@ def select_action(state: State, policy_net: HyperGraphGNN, e: float, greedy: boo
             action        = possible_idx[index].item()
     else:
         action = random.choice(possible_actions).id
-    LB, UB = [(a.lb, a.ub) for a in possible_actions if a.id == action][0]
-    return torch.tensor([[action]], device=device, dtype=torch.long), LB, UB
+    LB = [a.lb for a in possible_actions if a.id == action][0]
+    return torch.tensor([[action]], device=device, dtype=torch.long), LB
 
 def _build_batch_indices(actions_local_indices: Tensor, nb_tasks :int, batch_size: int):
     graph_offsets: Tensor = torch.arange(batch_size, device=actions_local_indices.device) * nb_tasks

src/replay_memory.py
@@ -6,7 +6,7 @@ from torch import Tensor
 from torch_geometric.data import HeteroData
 from torch._prims_common import DeviceLikeType
 
-from conf import MEMORY_CAPACITY, W_UB, W_LB, W_FINAL
+from conf import MEMORY_CAPACITY, W_LB, W_FINAL
 
 # ====================================================
 # =*= Model file for GNN tree-shaped replay memory =*=
@@ -19,21 +19,18 @@ __license__ = "MIT License"
 class PossibleAction:
     id: int
     lb: int
-    ub: int
 
     def __repr__(self):
-        return f'PA(id={self.id}, lb={self.lb}, ub={self.ub})'
+        return f'PA(id={self.id}, lb={self.lb})'
 
 class Transition:
     """
         One transition in the DRL MEMORY TREE
     """
-    def __init__(self, action: Tensor, previous_graph: HeteroData, graph: HeteroData, lb: int, ub: int, delta_lb: int, delta_ub :int, possible_actions: list[PossibleAction], parent=None):
+    def __init__(self, action: Tensor, previous_graph: HeteroData, graph: HeteroData, lb: int, delta_lb: int, possible_actions: list[PossibleAction], parent=None):
         self.action: Tensor               = action
         self.graph: HeteroData            = graph.clone().to('cpu')
         self.delta_lb: int                = delta_lb
-        self.delta_ub: int                = delta_ub
-        self.ub: int                      = ub
         self.lb: int                      = lb
         self.previous_graph: HeteroData   = previous_graph.clone().to('cpu')
         self.parent: Transition           = parent
@@ -61,11 +58,10 @@ class Transition:
         return self.parent == t.parent and torch.equal(self.action, t.action)
     
     def compute_reward(self, makespan: int, device: DeviceLikeType):
-        r: float       = (-1.0) * ((makespan * W_FINAL) + (self.delta_lb * W_LB) + (self.delta_ub * W_UB))
+        r: float       = (-1.0) * ((makespan * W_FINAL) + (self.delta_lb * W_LB))
         self.reward    = torch.tensor([r], device=device)
         self.makespan  = makespan
         self.lb        = min(self.lb, makespan)
-        self.ub        = max(self.ub, makespan)
         self.nb_visits = 1
         self.makespans.append(makespan)
 
@@ -77,11 +73,9 @@ class Transition:
             self.reward   = t.reward
             self.makespan = t.makespan
 
-    def refine_from_possible_children(self, init_lb: int, init_ub: int):
+    def refine_from_possible_children(self, init_lb: int):
         self.lb               = min(a.lb for a in self.possible_actions)
-        self.ub               = min(a.ub for a in self.possible_actions)
         self.delta_lb         = (self.lb - self.parent.lb) if self.parent is not None else (self.lb - init_lb)
-        self.delta_ub         = (self.parent.ub - self.ub) if self.parent is not None else (init_ub - self.ub)
 
 class ITree:
     """